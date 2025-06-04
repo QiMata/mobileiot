90b9a0fc182136280baf74420a9c57306b313318
Jared Rhodes <jrhodes@qimata.com>
2025-06-04 18:47:54 -0400

    Running on the demo phone with crashes

--- src/MobileIoT/QiMata.MobileIoT/MainPage.xaml.cs
+++ src/MobileIoT/QiMata.MobileIoT/MainPage.xaml.cs
@@ -6,18 +6,46 @@ namespace QiMata.MobileIoT
     {
         public ICommand NavigateCommand { get; }
 
+        readonly Dictionary<string, Func<Task<bool>>> _permissionChecks;
+
         public MainPage()
         {
             InitializeComponent();
 
-            NavigateCommand = new Command<string>(async (pageName) => await NavigateToPage(pageName));
+            _permissionChecks = new Dictionary<string, Func<Task<bool>>>
+            {
+                ["BlePage"] = EnsurePermission<Permissions.LocationWhenInUse>,
+                ["BleScannerPage"] = EnsurePermission<Permissions.LocationWhenInUse>,
+                ["NfcPage"] = () => Task.FromResult(true),
+                ["NfcP2PPage"] = () => Task.FromResult(true),
+                ["UsbPage"] = () => Task.FromResult(true),
+                ["SerialPage"] = () => Task.FromResult(true),
+                ["WifiDirectPage"] = EnsurePermission<Permissions.NetworkState>
+            };
+
+            NavigateCommand = new Command<string>(async route => await NavigateToPage(route));
 
             BindingContext = this;
         }
 
-        private async System.Threading.Tasks.Task NavigateToPage(string pageName)
+        static async Task<bool> EnsurePermission<TPermission>() where TPermission : Permissions.BasePermission, new()
         {
-            await Shell.Current.GoToAsync(pageName);
+            var status = await Permissions.CheckStatusAsync<TPermission>();
+            if (status != PermissionStatus.Granted)
+                status = await Permissions.RequestAsync<TPermission>();
+
+            return status == PermissionStatus.Granted;
+        }
+
+        async Task NavigateToPage(string route)
+        {
+            if (_permissionChecks.TryGetValue(route, out var check) && !await check())
+            {
+                await DisplayAlert("Permissions required", "Required permissions were not granted.", "OK");
+                return;
+            }
+
+            await Shell.Current.GoToAsync(route);
         }
     }
 }
